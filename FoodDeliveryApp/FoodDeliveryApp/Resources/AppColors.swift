a484b2026a3992aa246a611c2fb24b0e237b1dc8
EvanLondonMusic <154972101+EvanLondonMusic@users.noreply.github.com>
2024-01-28 23:01:00 +0300

    Added new base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  FoodDeliveryApp
+//
+//  Created by Evan London on 28.01.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor().hex(0xD35400)
+    static let grey = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xFFFFFF)
+}
