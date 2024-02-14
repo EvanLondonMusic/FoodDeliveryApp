7832458f2cce622d9c51cffb592d87f4cc91eec3
EvanLondonMusic <154972101+EvanLondonMusic@users.noreply.github.com>
2024-02-14 21:28:06 +0300

    Added Factory

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+// MARK: - OnboardingCoordinator
 class OnboardingCoordinator: Coordinator {
     
+    //MARK: - Properties
+    private let factory = SceneFactory.self
+    
+    // MARK: - Methods
     override func start() {
         showOnboarding()
     }
@@ -29,41 +34,11 @@ class OnboardingCoordinator: Coordinator {
 //    }
 //}
 
+
+// MARK: - Navigation
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [OnboardingPartViewController]()
-        
-        let firstVC = OnboardingPartViewController()
-        firstVC.imageToShow = UIImage(resource: .chickenLeg1)
-        firstVC.titleText = "Delicious Food"
-        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        firstVC.buttonText = "Next"
-        
-        let secondVC = OnboardingPartViewController()
-        secondVC.imageToShow = UIImage(resource: .shipped1)
-        secondVC.titleText = "Fastt Shipping"
-        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
-        secondVC.buttonText = "Next"
-        
-        let thirdVC = OnboardingPartViewController()
-        thirdVC.imageToShow = UIImage(resource: .medal1)
-        thirdVC.titleText = "Certificate Food"
-        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
-        thirdVC.buttonText = "Next"
-        
-        let fourVC = OnboardingPartViewController()
-        fourVC.imageToShow = UIImage(resource: .creditCard1)
-        fourVC.titleText = "Payment Online"
-        fourVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
-        fourVC.buttonText = "Cool!"
-        
-        pages.append(firstVC)
-        pages.append(secondVC)
-        pages.append(thirdVC)
-        pages.append(fourVC)
-        
-        let presenter = OnboardingViewPresenter(coordinator: self)
-        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        let viewController = factory.makeOnbordingScene(coordinator: self)
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
